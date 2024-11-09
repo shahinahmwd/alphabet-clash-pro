a28cb040c53792d37f67218d21fbb4f1b84fbae8
mdshahinahmed <smshahinahmed4115@gmail.com>
2024-11-09 16:07:11 +0600

    added keyup

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -6,10 +6,32 @@
 //     const playGround = document.getElementById('play-ground');
 //     playGround.classList.remove('hidden');
 // }
+
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+
+    //key player is expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet'); 
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    // check right or wrong key pressed
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        console.log('you have pressed correctly',expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you try again');
+    }
+}
+document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+
 function continueGame() {
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
 
     // set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
